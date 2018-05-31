9185c8410f653467841de781925d75d753c47fab
Fangyuan Tan <30452426+contr4l@users.noreply.github.com>
2018-06-01 01:24:09 +1000

    Analyze and export python file.

---
+++ ReadCSV.py
@@ -0,0 +1,74 @@
+import csv
+import time
+import re
+
+def write_chat_txtfile():
+    chat_txtfile = open(r'C:\Users\ctrl\Desktop\chat.txt','w',encoding='gb18030')
+    count = 0
+    with open(r'C:\Users\ctrl\Desktop\chathistory.csv','r',encoding='gb18030') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            newlist = []
+            if row['talker'] == 'chhhhhhensy' and row['imgPath'] == '' and 'content' not in row['content']:
+                if row['isSend'] == '1':
+                    time_str = str(time.gmtime(int(row['createTime'])/10e2))
+                    match_list = re.findall('=(\d+)', time_str)
+                    match_list[3] = str(int(match_list[3]) + 8)
+                    for i in match_list:
+                        if len(i) == 1:
+                            newlist.append('0'+i)
+                        else:
+                            newlist.append(i)
+                    time_standard = '{0}-{1}-{2} {3}:{4}:{5}'.format(newlist[0],newlist[1],newlist[2],newlist[3],newlist[4],newlist[5])
+                    chat_txtfile.write(time_standard + '       哥哥：' + row['content'] + '\n')
+                else:
+                    time_str = str(time.gmtime(int(row['createTime'])/10e2))
+                    match_list = re.findall('=(\d+)', time_str)
+                    match_list[3] = str(int(match_list[3]) + 8)
+                    for i in match_list:
+                        if len(i) == 1:
+                            newlist.append('0'+i)
+                        else:
+                            newlist.append(i)
+                    time_standard = '{0}-{1}-{2} {3}:{4}:{5}'.format(newlist[0], newlist[1], newlist[2],newlist[3], newlist[4], newlist[5])
+                    chat_txtfile.write(time_standard + '       宝贝：' + row['content'] + '\n')
+                count += 1
+                print(count)
+
+def write_sorted_chathistory():
+    chat_txtfile = open(r'C:\Users\ctrl\Desktop\chat_tk.txt', 'w', encoding='gb18030')
+    count = 0
+    chathistory_dict = {}
+    with open(r'C:\Users\ctrl\Desktop\chathistory.csv', 'r', encoding='gb18030') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            newlist = []
+            if row['talker'] == 'tiankun_007' and row['imgPath'] == '' and 'content' not in row['content']:
+                if row['isSend'] == '1':
+                    time_str = str(time.gmtime(int(row['createTime']) / 10e2 + 36000))
+                    match_list = re.findall('=(\d+)', time_str)
+                    for i in match_list:
+                        if len(i) == 1:
+                            newlist.append('0' + i)
+                        else:
+                            newlist.append(i)
+                    time_standard = '{0}-{1}-{2} {3}:{4}:{5}'.format(newlist[0], newlist[1], newlist[2], newlist[3],
+                                                                     newlist[4], newlist[5])
+                    chathistory_dict[row['createTime']] = time_standard + '       园小方：' + row['content'] + '\n'
+                else:
+                    time_str = str(time.gmtime(int(row['createTime']) / 10e2 + 36000))
+                    match_list = re.findall('=(\d+)', time_str)
+                    for i in match_list:
+                        if len(i) == 1:
+                            newlist.append('0' + i)
+                        else:
+                            newlist.append(i)
+                    time_standard = '{0}-{1}-{2} {3}:{4}:{5}'.format(newlist[0], newlist[1], newlist[2], newlist[3],
+                                                                     newlist[4], newlist[5])
+                    chathistory_dict[row['createTime']] = time_standard + '       古叶田：' + row['content'] + '\n'
+                count += 1
+                print(count)
+    chat_history = sorted(chathistory_dict.values())
+    for i in chat_history:
+        chat_txtfile.write(i)
+write_sorted_chathistory()
---
+++ analyze.py
@@ -0,0 +1,61 @@
+import jieba.analyse
+import string
+filename = r'C:\Users\ctrl\Desktop\chat_tk'
+
+
+# 此模块用于分析文件中的字频，输出结果形如 词语 --- 权重频次
+def AnalyzeData():
+    f = open(filename + '.txt', 'r', encoding='gb18030')
+    fcontent = f.read()
+    alpha = 'qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM'
+    tags = jieba.analyse.extract_tags(fcontent, topK=250, withWeight=True)
+    new_tags = {}
+    for k in range(len(tags)):
+        uchar = tags[k][0][0]
+        if uchar not in alpha:
+            new_tags[tags[k][0]]= int(tags[k][1]*10000)
+
+    # 将词频-词语保存为文件，注意格式化对齐的方式
+    with open(filename + '_Word.txt', 'w') as f:
+        for i, j in tags:
+            if i[0] not in alpha:
+                f.write('{:15}\t{:15}'.format(i,int(j*10000))+'\n')
+            # print('{:8}\t{:10}'.format(i,int(j*10000)))
+        f.close()
+
+    # 返回字典为wordcloud提供依据
+    return new_tags
+
+from wordcloud import WordCloud
+from scipy.misc import imread
+import wordcloud
+
+def cloudplot():
+    # 设置模板图像的路径
+    target_coloring = imread(r'C:\Users\ctrl\Desktop\heart.jpg')
+    # 以词频和背景模板为依据生成词云对象
+    word_cloud = WordCloud(font_path = r'C:\Windows\Fonts\simhei.ttf',
+                           background_color="white",max_words=2000,mask=target_coloring).generate_from_frequencies(AnalyzeData())
+    # 生成颜色分布
+    image_color = wordcloud.ImageColorGenerator(target_coloring)
+    # image_color =
+
+    import matplotlib.pyplot as plt
+    # 仅按照词频、边界、默认颜色生成词云图像
+    plt.imshow(word_cloud)
+    plt.axis("off")
+    plt.figure()
+
+    # 重新上色，按照图像色彩分布生成
+    plt.imshow(word_cloud.recolor(color_func=image_color))
+    plt.axis("off")
+    plt.figure()
+
+    # 绘制原始图像
+    plt.imshow(target_coloring,cmap=plt.cm.gray)
+    plt.axis("off")
+    plt.show()
+
+    word_cloud.to_file(filename+'.png')
+    
+cloudplot()
